1c53df584499e138675f6a0f9b71401007acec2e
yatoenough <yatoenough@gmail.com>
2024-07-02 17:41:18 +0200

    configured main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	const port = +process.env.PORT || 2999;
+
+	const config = new DocumentBuilder()
+		.setTitle('Chat API')
+		.setDescription('Chat app realtime API')
+		.setVersion('1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
+	app.use(helmet());
 	app.enableCors();
+	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 	app.setGlobalPrefix('/api');
-  
-	await app.listen(3000);
+	console.log('PORT:' + port);
+
+	await app.listen(port);
 }
 bootstrap();
